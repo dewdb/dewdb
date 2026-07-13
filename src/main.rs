9e807a3be33889de9027a78f1bacb7d23c5b0d29
viveke22 <vivek.lead1@gmail.com>
2026-07-13 21:29:57 +0530

    add collection storage layer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
-use std::fs::{self, File};
+use std::fs::{self, File, OpenOptions};
 use std::path::{Path, PathBuf};
+use std::sync::{Mutex, RwLock};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 const WAL_ROTATION_LIMIT: u64 = 50 * 1024 * 1024;
@@ -35,6 +36,19 @@ struct IndexSnapshot {
     map: BTreeMap<String, IndexEntry>,
 }
 
+struct WalsState {
+    current_wal: File,
+    current_wal_id: u64,
+    current_wal_size: u64,
+}
+
+struct Collection {
+    name: String,
+    root_path: PathBuf,
+    index: RwLock<BTreeMap<String, IndexEntry>>,
+    wal_writer: Mutex<WalsState>,
+}
+
 struct Database {
     root_path: PathBuf,
 }
@@ -57,14 +71,26 @@ fn current_timestamp() -> u64 {
 fn main() -> std::io::Result<()> {
     let db = Database::new("./data")?;
 
-    let wal_path = db.root_path.join("wal-00001.log");
+    let collection_path = db.root_path.join("users");
+    fs::create_dir_all(&collection_path)?;
 
-    let wal = File::create(&wal_path)?;
+    let wal_path = collection_path.join("wal-00001.log");
 
-    let mut snapshot = IndexSnapshot {
-        last_wal_id: 1,
-        last_offset: 0,
-        map: BTreeMap::new(),
+    let wal = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .read(true)
+        .open(&wal_path)?;
+
+    let collection = Collection {
+        name: "users".into(),
+        root_path: collection_path,
+        index: RwLock::new(BTreeMap::new()),
+        wal_writer: Mutex::new(WalsState {
+            current_wal: wal,
+            current_wal_id: 1,
+            current_wal_size: 0,
+        }),
     };
 
     let entry = LogEntry::Put {
@@ -75,18 +101,33 @@ fn main() -> std::io::Result<()> {
         ts: current_timestamp(),
     };
 
-    snapshot.map.insert(
-        "example".into(),
-        IndexEntry {
-            wal_id: 1,
-            offset: 0,
-        },
-    );
+    {
+        let mut index = collection.index.write().unwrap();
+
+        index.insert(
+            "example".into(),
+            IndexEntry {
+                wal_id: 1,
+                offset: 0,
+            },
+        );
+    }
+
+    let snapshot = IndexSnapshot {
+        last_wal_id: 1,
+        last_offset: 0,
+        map: collection.index.read().unwrap().clone(),
+    };
+
+    let wal = collection.wal_writer.lock().unwrap();
 
     println!("{entry:?}");
     println!("{snapshot:#?}");
-    println!("Created {:?}", wal_path);
-    println!("File size: {}", wal.metadata()?.len());
+    println!("Collection: {}", collection.name);
+    println!("Root: {:?}", collection.root_path);
+    println!("WAL id: {}", wal.current_wal_id);
+    println!("WAL size: {}", wal.current_wal_size);
+    println!("Current file size: {}", wal.current_wal.metadata()?.len());
 
     println!(
         "{} {} {}",
