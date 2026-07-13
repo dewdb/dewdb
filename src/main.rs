c537db42be0e232760c40ab6f94768b3cd5bbdec
viveke22 <vivek.lead1@gmail.com>
2026-07-13 21:12:35 +0530

    add log entry model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "op", rename_all = "lowercase")]
+enum LogEntry {
+    Put {
+        key: String,
+        value: serde_json::Value,
+        ts: u64,
+    },
+    Del {
+        key: String,
+        ts: u64,
+    },
+}
+
 fn main() {
-    println!("dewdb");
+    let entry = LogEntry::Put {
+        key: "example".into(),
+        value: serde_json::json!({
+            "message": "dewdb"
+        }),
+        ts: 0,
+    };
+
+    println!("{entry:?}");
 }
